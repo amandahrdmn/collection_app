46412c89c43f3cc54517a0c9e7b89a93d912a448
Amanda Hardman <amandahrdmn@yahoo.com>
2020-03-23 15:31:55 +0000

    added html/css to display db entries

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+$db = new PDO('mysql:host=DB;dbname=collection_app', 'root', 'password');
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$plant_query = $db->prepare("SELECT `science_name`,`name`,`plant_types`.`type` FROM `plants` LEFT JOIN `plant_types` ON `plants`.`type` = `plant_types`.`id`;");
+$plant_query->execute();
+$plant_data=$plant_query->fetchAll();
+?>
+<!doctype html>
+<html lang='en'>
+<head>
+    <title>collection_app_ahardman</title>
+    <link href='normalize.css' rel='stylesheet' type='text/css'>
+    <link href='collection_app_ahardman_stylesheet.css' rel='stylesheet' type='text/css'>
+    <meta name="viewport" content="width=device-width">
+</head>
+
+<section class='entries'>
+    <div class='container'>
+        <div class='entry_container'>
+                <?php
+                foreach ($plant_data as $entry) {
+                    echo "<div class='entry_box'>
+                                <div class='entry'>" . $entry['science_name'] . "</div><div class='entry'>" . $entry['name'] . "</div><div class='entry'>" . $entry['type'] . "</div></div>";
+                }
+                ?>
+        </div>
+    </div>
