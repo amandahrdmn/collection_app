0846480888bb5bc972d5e62d4910785e25157dca
Amanda Hardman <amandahrdmn@yahoo.com>
2020-03-24 10:45:38 +0000

    updated unittest files

---
+++ functions.php
@@ -0,0 +1,42 @@
+<?php
+
+function getDB () {
+    $db = new PDO('mysql:host=DB;dbname=collection_app', 'root', 'password');
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $plant_query = $db->prepare("SELECT * FROM `plants` LEFT JOIN `plant_types` ON `plants`.`type` = `plant_types`.`id`;");
+    $plant_query->execute();
+
+    return $plant_query;
+}
+
+function makeAllTiles ($query) {
+    $plantEntry = getDataEntry($query);
+    while ($plantEntry) {
+        makePlantEntryTile($plantEntry);
+        $plantEntry = getDataEntry($query);
+    }
+}
+
+function getDataEntry(object $dataInput): array {
+    if (!empty($dataInput)) {
+        if (gettype($dataInput) === 'object') {
+            $entry = $dataInput->fetch();
+            return $entry;
+        } else {
+            echo 'Please input the correct data type';
+        }
+    } else {
+        echo 'There is no data for this collection.';
+    }
+}
+
+function makePlantEntryTile (array $entry) {
+    if (empty($entry)) {
+        echo 'There is no data for this entry';
+    } elseif (count($entry) < 4) {
+        echo 'There is not enough data for this entry';
+    } elseif (!empty($entry)) {
+        echo "<div class='entry_box'><div class='entry science_name'>" . $entry['science_name'] . "</div><div class='entry'>" . $entry['name'] . "</div><div class='entry'>" . $entry['type'] . "</div></div>";
+    }
+}
--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-session_start();
-$db = new PDO('mysql:host=DB;dbname=collection_app', 'root', 'password');
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$plant_query = $db->prepare("SELECT `science_name`,`name`,`plant_types`.`type` FROM `plants` LEFT JOIN `plant_types` ON `plants`.`type` = `plant_types`.`id`;");
-$plant_query->execute();
-$plant_data=$plant_query->fetchAll();
+session_start();
+
+require('functions.php');
+
+$plant_query = getDB ();
+
 ?>
 <!doctype html>
-<html lang='en'>
+<html lang='en' font-family = 'Segoe UI, Helvetica, Verdana'>
 <head>
     <title>collection_app_ahardman</title>
     <link href='normalize.css' rel='stylesheet' type='text/css'>
@@ -21,10 +21,7 @@ $plant_data=$plant_query->fetchAll();
         <h2>Plant Collection:</h2>
         <div class='entry_container'>
                 <?php
-                foreach ($plant_data as $entry) {
-                    echo "<div class='entry_box'>
-                                <div class='entry science_name'>" . $entry['science_name'] . "</div><div class='entry'>" . $entry['name'] . "</div><div class='entry'>" . $entry['type'] . "</div></div>";
-                }
+                    makeAllTiles($plant_query);
                 ?>
         </div>
     </div>
