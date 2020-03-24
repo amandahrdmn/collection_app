c3c9e6c084afe92da2fb2b63c8d031194db38ac9
Amanda Hardman <amandahrdmn@yahoo.com>
2020-03-24 15:52:01 +0000

    added baseline taken from login exercise to addpage.php

--- addpage.php
+++ addpage.php
@@ -1,2 +1,17 @@
+<label for="add_entry">User Login</label>
+<form id='add_entry' action="entry_check.php" method="POST" >
+    <div>
+        <input type="text" name="user" placeholder="Scientific Name">
+    </div>
+    <div>
+        <input type="password" name="password" placeholder="Password">
+    </div>
+    <div>
+        <input type="submit" value="Login">
+    </div>
+</form>
+
 <?php
 ?>
+
+
