57cbd9456d09e405ead062ea4a594d05ac1e2dd6
Amanda Hardman <amandahrdmn@yahoo.com>
2020-03-24 11:40:38 +0000

    removed $actual from function calls with no output

--- test/functions.php
+++ test/functions.php
@@ -6,18 +6,18 @@ class collection_appTest extends TestCase
      public function testSuccessMakePlantEntryTile () {
         $expected = "<div class='entry_box'><div class='entry science_name'>" . "Querbus robur" . "</div><div class='entry'>" . "English Oak" . "</div><div class='entry'>" . "Tree" . "</div></div>";
         $this->expectOutputString($expected);
-        $actual = makePlantEntryTile(['id' => "1", 'science_name' => "Querbus robur", 'name' => "English Oak", 'type' => "Tree"]);
+        makePlantEntryTile(['id' => "1", 'science_name' => "Querbus robur", 'name' => "English Oak", 'type' => "Tree"]);
     }
 
     public function testErrorMakePlantEntryTileTooShort () {
         $expected = 'There is not enough data for this entry';
         $this->expectOutputString($expected);
-        $actual = makePlantEntryTile(['id' => "1", 'science_name' => "Querbus robur", 'name' => "English Oak"]);
+        makePlantEntryTile(['id' => "1", 'science_name' => "Querbus robur", 'name' => "English Oak"]);
     }
 
     public function testErrorMakePlantEntryTileEmptyInput () {
         $expected = 'There is no data for this entry';
         $this->expectOutputString($expected);
-        $actual = makePlantEntryTile([]);
+        makePlantEntryTile([]);
     }
 }
