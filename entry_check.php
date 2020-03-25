aa91c05b552f680779f922d670062a07de871e5c
Amanda Hardman <amandahrdmn@yahoo.com>
2020-03-25 14:48:38 +0000

    validated user input and added data to DB

--- entry_check.php
+++ entry_check.php
@@ -1,27 +1,31 @@
 <?php
+require_once('functions.php');
 session_start();
 
+$db = getDB();
 $science_name = $_POST['science_name'];
 $name = $_POST['name'];
 $type = $_POST['type'];
 
-$db = new PDO('mysql:host=DB;dbname=collection_app', 'root', 'password');
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-$nonunique_entry_query = $db->prepare("SELECT `id` FROM `plants` WHERE `science_name` = ? AND `name` = ?;");
-$nonunique_entry_query->execute([$science_name, $name]);
-$nonunique_entry = $nonunique_entry_query->fetch();
-var_dump($nonunique_entry);
-
-$used_type_query = $db->prepare("SELECT `id` FROM `plant_types` WHERE upper(?) LIKE `type`;");
-$used_type_query->execute([$type]);
-$used_type = $used_type_query->fetch();
-var_dump($used_type);
-
-if($nonunique_entry === false && !empty($used_type)) {
-    $add_entry_query = $db->prepare("INSERT INTO `plants` (`science_name`,`name`,`type`) VALUES (?,?,?);");
-    $add_entry_query->execute([$science_name, $name, $used_type]);
-    header('Location: index.php?entry_add_successful=1');
-} elseif (empty($used_type)) {
+if (empty($science_name) || empty($name) || empty($type)) {
     header('Location: addpage.php?entry_add_error=1');
+} else {
+    $nonunique_entry = checkforUniqueAddEntry($db, $science_name, $name);
+    $used_type = checkForWrongPlantType($db, $type);
+
+    if($nonunique_entry === false && $used_type !== ['not found']) {
+        $dbInsertCheck = insertDataToDB($db, $science_name, $name, $used_type);
+        if ($dbInsertCheck) {
+            header('Location: index.php?entry_add_successful=1');
+        } else {
+            header('Location: index.php?entry_add_error=2');
+        }
+    } elseif ($used_type === ['not found']) {
+        header('Location: addpage.php?entry_add_error=3');
+    } elseif ($nonunique_entry === true) {
+        header('Location: addpage.php?entry_add_error=4');
+    }
 }
+
+
+
--- index.php
+++ index.php
@@ -1,19 +1,17 @@
 <?php
 require_once('functions.php');
 
-$plant_data = getDB();
+$plant_data = getPlantData(getDB());
 
 echo DBCheck($plant_data);
-
-
 
 ?>
 <!doctype html>
 <html lang='en'>
 <head>
     <title>collection_app_ahardman</title>
-    <link href='collection_app_ahardman_stylesheet.css' rel='stylesheet' type='text/css'>
     <link href='normalize.css' rel='stylesheet' type='text/css'>
+    <link href='collection_app_ahardman_stylesheet.css' rel='stylesheet' type='text/css'>
     <meta name="viewport" content="width=device-width">
 </head>
 
